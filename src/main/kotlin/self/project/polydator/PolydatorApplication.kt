b160b30d635b1d9cbb4903eb97b1d80d6bc504e3
chih-chen <chih.chen1029@gmail.com>
2018-11-22 20:49:00 -0200

    initial project setup

---
+++ src/main/kotlin/self/project/polydator/PolydatorApplication.kt
@@ -0,0 +1,11 @@
+package self.project.polydator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolydatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<PolydatorApplication>(*args)
+}
