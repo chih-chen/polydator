ef84adcc9071c1720f9f8e5097feef1dca98e922
chih-chen <chih.chen1029@gmail.com>
2018-11-22 22:42:55 -0200

    add entity and usecase modules

--- web/src/main/kotlin/self/project/polydator/controllers/CategorizationController.kt
+++ web/src/main/kotlin/self/project/polydator/controllers/CategorizationController.kt
@@ -1,14 +1,14 @@
-package self.project.polydator
+package self.project.polydator.controllers
 
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/categorize")
 class CategorizationController {
 
-    @RequestMapping("/triangle", method = [POST])
+    @PostMapping("/triangle")
     fun categorizeTriangle(coordinates: Triple<Int, Int, Int>): String {
         return "ok"
     }
