563133eb320df9a9354f16d80d3bc027c40b8d34
chih-chen <chih.chen1029@gmail.com>
2018-12-12 22:35:43 -0200

    upgrade gradle and change resolveShape return type

--- service/src/main/kotlin/self/project/polydator/resolver/PolygonResolverImpl.kt
+++ service/src/main/kotlin/self/project/polydator/resolver/PolygonResolverImpl.kt
@@ -1,6 +1,7 @@
 package self.project.polydator.resolver
 
 import org.springframework.stereotype.Service
+import self.project.polydator.Polygon
 import self.project.polydator.categorization.gateway.PolygonResolver
 import self.project.polydator.exception.UnsupportedPolygonException
 import self.project.polydator.triangle.Triangle
@@ -8,7 +9,7 @@ import self.project.polydator.triangle.Triangle
 @Service
 class PolygonResolverImpl : PolygonResolver {
 
-    override fun resolveShape(sides: List<Int>): Triangle {
+    override fun resolveShape(sides: List<Int>): Polygon {
         return when (sides.size) {
             3 -> Triangle(a = sides[0], b = sides[1], c = sides[2])
             else -> throw UnsupportedPolygonException()
